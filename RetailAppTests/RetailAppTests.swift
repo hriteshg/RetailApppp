26ba717ac2258ca1422d1f3ff45a9e81814a86e6
hritesh <hritesh.g@everest.engineering>
2022-01-21 17:58:27 +0530

    Initial Commit

---
+++ RetailAppTests/RetailAppTests.swift
@@ -0,0 +1,33 @@
+//
+//  RetailAppTests.swift
+//  RetailAppTests
+//
+//  Created by Hritesh on 21/01/22.
+//
+
+import XCTest
+@testable import RetailApp
+
+class RetailAppTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
